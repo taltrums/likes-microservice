56af7aad9f072ef8c745a5d32ba76894918bea03
taltrums <sidekicktal@outlook.com>
2023-06-29 10:27:12 +0530

    update db uri

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/likes_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@flask_db:5432/likes_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'redis'
     CACHE_REDIS_URL = 'redis://localhost:6379/0'
