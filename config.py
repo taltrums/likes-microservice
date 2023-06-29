1ac31e24f4eb74caf1430a52b4797e151f3b7b60
taltrums <sidekicktal@outlook.com>
2023-06-29 11:34:55 +0530

    fix redis url

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@flask_db:5432/likes_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'redis'
-    CACHE_REDIS_URL = 'redis://localhost:6379/0'
+    CACHE_REDIS_URL = 'redis://redis:6379/0'
